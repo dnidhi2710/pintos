f9d4370e05626ec56a55375e4bd36fada1e5b7c1
Dhayanidhi <dnidhi2710@gmail.com>
2018-10-20 06:44:48 -0400

    palloc

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -460,6 +460,7 @@ void check_for_donation(){
     struct thread *main_thread = list_entry (list_front (&ready_list), struct thread, elem);
     if(thread_current()->priority > main_thread->priority){
         struct donation *t;
+        t = palloc_get_page (PAL_ZERO);
         //strlcpy (t->donor, thread_current()->name, sizeof thread_current()->name);
         //strlcpy (t->donee, main_thread->name, sizeof main_thread->name);
         t->previous_priority = main_thread->donated_priority!=0 ? main_thread->donated_priority:  main_thread->priority;
